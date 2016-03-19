db8fd9fb19a3fbb16dc7074c8905b1c2f771d677
Avinash S Herle <avinash.herle04@gmail.com>
2016-03-18 17:15:50 -0700

    Commiting files

--- Q1.scala
+++
@@ -1,6 +0,0 @@
-val filePath = "hdfs://cshadoop1/yelpdatafall/business/business.csv"
-val location = readLine("Enter location")
-val logData = sc.textFile(filePath)
-val lines = logData.filter(line => line.contains(location)).map(_.split("\\^")(0)).distinct
-lines.foreach(println)
-
